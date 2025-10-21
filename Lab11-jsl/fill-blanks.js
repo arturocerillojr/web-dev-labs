ef47ae6f2aaae47162c16982a86d2c5a65ac5970
Arturo Cerillo Jr. <arturocerillojr@gmail.com>
2025-10-21 21:29:17 +0800

    generela-commit-m11

---
+++ Lab11-jsl/fill-blanks.js
@@ -0,0 +1,3 @@
+let name = "Arturo";
+let age = "20";
+console.log("Hi, my name is" + name +"and I am"+ 20 + "years old.");
---
+++ Lab11-jsl/fix.js
@@ -0,0 +1,5 @@
+// JavaScript fix or confirmation of fix
+document.addEventListener("DOMContentLoaded", function () {
+  const box = document.getElementById("fix-box");
+  console.log("Box classes applied:", box.className);
+});
